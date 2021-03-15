74d4978a0e7c8474271a064002fd4309e4d13ea1
Ccw0925 <kidoccw@gmail.com>
2021-03-15 20:22:47 +0800

    Git initialization

---
+++ App.js
@@ -0,0 +1,226 @@
+import React, {Component, useState} from 'react';
+import { StyleSheet, Text, View, FlatList, Dimensions, SafeAreaView, Image, Button, Modal, TouchableOpacity} from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+// import * as firebase from 'firebase';
+
+// var firebaseConfig = {
+//   apiKey: "AIzaSyAS_d1ZN-cErWHrQardjeSLrqXsBofBSws",
+//   authDomain: "vendingmachine-e55bc.firebaseapp.com",
+//   databaseURL: "https://vendingmachine-e55bc.firebaseio.com",
+//   projectId: "vendingmachine-e55bc",
+//   storageBucket: "vendingmachine-e55bc.appspot.com",
+//   messagingSenderId: "532545534846",
+//   appId: "1:532545534846:web:4e4433a8853581a00a112f",
+//   measurementId: "G-2DWNMMY7V8"
+// };
+
+// // Initialize Firebase
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+// }
+// const getQrcode = (codeId) => {
+//   firebase.database().ref('Qrcode/' + codeId).on('value', (snapshot) => {
+//     const codeStr = snapshot.val();
+//     console.log("Code String: " + codeStr);
+//   });
+// }
+const dataList = [{name: 'Coca-Cola', image: 'https://cached.imagescaler.hbpl.co.uk/resize/scaleWidth/888/cached.offlinehbpl.hbpl.co.uk/news/ORP/Coke-20150730041122131.JPG',price: 1.70}, 
+                  {name: 'Sprite', image: 'https://trottospizza.com/wp-content/uploads/2017/11/Sprite_Can_375ml_1_1024x1024-e1443056044254.png',price: 2.30}, 
+                  {name: '100 Plus', image: 'https://www.pantryexpress.my/27-thickbox_default/100-plus-325ml-x-24-original.jpg', price: 1.40}];
+const WIDTH = Dimensions.get('window').width
+const numColumns = 2
+
+const leftTop = {
+  borderLeftWidth: 4,
+  borderTopWidth: 4,
+  borderColor: 'white'
+};
+const leftBottom = {
+  borderLeftWidth: 4,
+  borderBottomWidth: 4,
+  borderColor: 'white'
+};
+const rightTop = {
+  borderRightWidth: 4,
+  borderTopWidth: 4,
+  borderColor: 'white'
+};
+const rightBottom = {
+  borderRightWidth: 4,
+  borderBottomWidth: 4,
+  borderColor: 'white'
+};
+
+const requestCameraPermission = async () => {
+  const { status } = await Permissions.askAsync(Permissions.CAMERA);
+  
+  setCamera(prevState => ({ ...prevState, hasCameraPermission: status === 'granted'}));
+
+};
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shouldShowQrScanner: false,
+      shouldShowModal: false,
+      scanned: false,
+      index: 0
+    }
+  }
+
+  formatData = (dataList, numColumns) => {
+    const totalRows = Math.floor(dataList.length / numColumns)
+    let totalLastRow = dataList.length - (totalRows * numColumns)
+
+    while (totalLastRow !== 0 && totalLastRow !== numColumns) {
+      dataList.push({key: 'blank', empty: true})
+      totalLastRow++
+    }
+    return dataList
+  }
+
+  _renderItem = ({item, index}) => {
+    let {itemStyle, itemText, itemInvisible} = styles
+    if(item.empty) {
+      return <View style={[itemStyle, itemInvisible]}/>
+    }
+    return(
+      <View style={itemStyle}>
+        <TouchableOpacity onPress={() => this.setState({ shouldShowModal: !this.state.shouldShowModal, index: index })}>
+          <Image 
+            style={{width: WIDTH / 2 - 10, height: 150, marginBottom: 5}}
+            resizeMode="contain"
+            source={{uri: item.image}}
+          />
+        </TouchableOpacity>
+          <Text style={itemText}>{item.name}</Text>
+      </View>
+    )
+  }
+
+  render() {
+    let {container, itemText, modalStyle, modalText, productDetails, buttonStyle, buttonFlex} = styles
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        {this.state.shouldShowQrScanner ? (<View style={{ flex: 1 }}>
+         <Camera
+         onBarCodeScanned={this.state.scanned ? undefined : console.log('test')}
+         style={StyleSheet.absoluteFillObject}
+      />
+      <View style={{ height: Dimensions.get('window').height / 2 - WIDTH / 4, width: WIDTH, backgroundColor: 'black', opacity: 0.5 }}></View>
+        <View style={{ height: WIDTH / 2, width: WIDTH, opacity: 0.5, flexDirection: 'row' }}>
+          <View style={{ flex: 1, backgroundColor: 'black' }}></View>
+          <View style={{ flex: 1 }}></View>
+          <View style={{ flex: 1 }}></View>
+          <View style={{ flex: 1, backgroundColor: 'black' }}></View>
+        </View>
+        <View style={{ height: Dimensions.get('window').height / 2 - WIDTH / 4, width: WIDTH, backgroundColor: 'black', opacity: 0.5 }}></View>
+        <View style={{ ...StyleSheet.absoluteFillObject, alignItems: 'center', justifyContent:'center' }}>
+          <View style={{ width: WIDTH / 2, height: WIDTH / 2 }}>
+            <View style={{ flex: 1, flexDirection: 'row' }}>
+              <View style={{ flex: 1, ...leftTop }}></View>
+              <View style={{ flex: 1 }}></View>
+              <View style={{ flex: 1, ...rightTop }}></View>
+            </View>
+            <View style={{ flex: 1 }}></View>
+            <View style={{ flex: 1, flexDirection: 'row' }}>
+              <View style={{ flex: 1, ...leftBottom }}></View>
+              <View style={{ flex: 1 }}></View>
+              <View style={{ flex: 1, ...rightBottom }}></View>
+            </View>
+          </View>
+          <Button 
+                color='red'
+                title="Back"
+                onPress={() => this.setState({ shouldShowQrScanner: !this.state.shouldShowQrScanner })}
+                />
+        </View>
+      </View>
+      ) : (<View style={container}>
+        <Modal visible={this.state.shouldShowModal}>
+          <View style={modalStyle}>
+            <Image 
+              style={{width: 300, height: 300, marginBottom: 5}}
+              resizeMode="contain"
+              source={{uri: dataList[this.state.index].image}}
+            />
+            <View style={productDetails}>
+              <View style={{paddingRight: 25}}>
+                <Text style={modalText}>Product Name:</Text>
+                <Text style={modalText}>Price:</Text>
+              </View>
+              <View style={{paddingLeft: 25}}>
+                <Text style={modalText}>{dataList[this.state.index].name}</Text>
+                <Text style={modalText}>RM{dataList[this.state.index].price.toFixed(2)}</Text>
+              </View>
+            </View>
+            <View style={buttonStyle}>
+              <View style={buttonFlex}>
+                <Button 
+                color='red'
+                title="Back"
+                onPress={() => this.setState({ shouldShowModal: !this.state.shouldShowModal })}
+                />
+              </View>
+              <View style={buttonFlex}>
+                <Button 
+                color='blue'
+                title="Pay"
+                onPress={() => this.setState({ shouldShowQrScanner: !this.state.shouldShowQrScanner })}
+                />
+            </View>
+            </View>
+          </View>
+        </Modal>
+        <FlatList 
+          data={this.formatData(dataList, numColumns)}
+          renderItem={this._renderItem}
+          keyExtractor={(item, index) => index.toString()}
+          numColumns={numColumns}
+        /></View>)}
+      </SafeAreaView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40
+  },
+  itemStyle: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 100,
+    flex: 1,
+    margin: 1,
+    height: WIDTH / numColumns
+  },
+  itemText: {
+    color: 'black',
+    fontSize: 20
+  },
+  itemInvisible: {
+    backgroundColor: 'transparent'
+  },
+  modalStyle: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  modalText: {
+    fontSize: 25
+  },
+  productDetails: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  buttonStyle: {
+    flexDirection: 'row'
+  },
+  buttonFlex: {
+    flex: 1
+  }
+});
